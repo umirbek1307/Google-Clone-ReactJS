b7a6cef3c470ac4e4b31fc451a8cd359e04ac1e6
Umirbek Murotov <46290887+umirbek1307@users.noreply.github.com>
2021-10-20 19:58:36 +0500

    Add files via upload

---
+++ pages/App.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import './App.css';
+import Home from './components/Home';
+import{ BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import SearchPage from './SearchPage';
+
+function App() {
+  return (
+    //BEM
+    <div className="App">
+      <Router>
+      <Switch>
+        <Route path ="/search">
+        <SearchPage />
+        </Route>
+        <Route path="/">
+          <Home/>
+        </Route>
+      </Switch>
+        </Router>
+     
+    </div>
+  );
+}
+
+export default App;
